ab743e712575e8d79b12290c33e2e6b3c8f6d6d4
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2020-01-08 11:06:16 -0600

    Minor styling changes

--- weight-lifting/src/components/Login.js
+++ weight-lifting/src/components/Login.js
@@ -20,11 +20,11 @@ const Login = props => {
 
 return (   
         
-        <div id="Login">
+        <div id="form">
             <form onSubmit={submitForm}>
 
                 <label htmlFor="Username">Username:</label>
-                <input id="username" type="text" placeholder="username" name="username" value={userCheck.username} onChange={handleChanges} noValidate/>
+                <input id="username" type="text" placeholder="" name="username" value={userCheck.username} onChange={handleChanges} noValidate/>
                     {/* {showError1 === true ? <span>User does not exist</span> : ""} */}
                 <label htmlFor="Password">Password:</label>
                 <input id="password" type="password" placeholder="" name="password" value={userCheck.password} onChange={handleChanges} noValidate/>
