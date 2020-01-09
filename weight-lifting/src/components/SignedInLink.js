d7e108fe48976129ba14392a5fc33df0b8f71268
Jacob Washburn <jacobrwashburn@gmail.com>
2020-01-09 11:59:37 -0700

    sign out removes userId from local storage

--- weight-lifting/src/components/SignedInLink.js
+++ weight-lifting/src/components/SignedInLink.js
@@ -5,7 +5,7 @@ const SignedInLink  = () => {
     return (
         <ul className="right">
             <li><NavLink to="/newworkoutform"> New Workout</NavLink></li>
-            <li onClick={() => {localStorage.removeItem("token")}} ><NavLink to="/login">Sign Out</NavLink></li>
+            <li onClick={() => {localStorage.removeItem("token");localStorage.removeItem("userId")}} ><NavLink to="/login">Sign Out</NavLink></li>
             <li><NavLink to="/dashboard" className=" btn btn-floating black"> Ray</NavLink></li>
         </ul>
     )
